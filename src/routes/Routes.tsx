57ae9683ad562eba19d5153d3cee210554dfc42f
allanmozart <mozart.fuzer@gmail.com>
2023-11-19 03:07:23 +0000

    added routes folder and link to github on footer

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "../components/LandingPage";
+
+export const AllRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
