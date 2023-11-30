99b14623c51a7a3533927bd5cf167e5d1f1eb753
allanmozart <mozart.fuzer@gmail.com>
2023-11-30 23:39:22 +0000

    removed unecessary comment

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import storage from "redux-persist/lib/storage";
 import promotionReducer from "./promotion/promotion";
 import cartReducer from "./cartSlice";
 
