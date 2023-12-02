5c676762a1eb85771b3c083bb909f7d00ac7cf74
allanmozart <mozart.fuzer@gmail.com>
2023-12-02 06:17:27 +0000

    vite config and test

--- src/components/LandingCarousel/landingCarousel.test.tsx
+++ src/components/LandingCarousel/landingCarousel.test.tsx
@@ -1,15 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
 import Carousel from "./index";
 
 jest.mock("../../api/API_PATH", () => ({
   getAllProducts: jest.fn(() => Promise.resolve([])),
 }));
 
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
 describe("Carousel Component", () => {
   it("renders", async () => {
-
     render(<Carousel />);
-    expect(screen.getByRole('button', {name: "left arrow button"})).toBeDefined();
+    expect(
+      screen.getByRole("button", { name: "left arrow button" })
+    ).toBeDefined();
+    expect(
+      screen.getByRole("button", { name: "right arrow button" })
+    ).toBeDefined();
   });
 });
